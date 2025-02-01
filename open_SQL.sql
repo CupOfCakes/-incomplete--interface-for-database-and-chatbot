b098c3075751f674dcd5f43aff32439b5f5f75cb
heitor <heitor.scarv@gmail.com>
2025-02-01 15:04:08 -0300

    Create open_SQL.sql

---
+++ open_SQL.sql
@@ -0,0 +1,8 @@
+create table circuitos_eletricos(
+id int primary key not null auto_increment,
+name_arquive varchar(100) not null,
+arquive_type varchar(100) not null,
+category varchar(100) not null,
+descricao varchar(250) null,
+semester int not null
+)
